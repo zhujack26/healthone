a51b970c06bb1b8c24061fdad14b116c8242e132
조원희 <onehee9710@gmail.com>
2023-04-20 17:45:32 +0900

    feat : 불편사항 접수 모달 생성 및 프론트 기능 추가

--- front/src/components/ReportManage/ReportAnswerModal.js
+++ front/src/components/ReportManage/ReportAnswerModal.js
@@ -1,19 +1,58 @@
 /* import react */
 
 /* import component */
+import {Button} from "@material-ui/core"
 
 /* import css */
 import "../../assets/css/ReportAnswerModal.css";
 
+/**
+ * 
+ * @param {Object} Report 불편사항 객체(title, name, email, date, content) 포함.
+ * @param {Object} closeModal 모달 닫는 함수
+ * @returns 
+ */
+const ReportAnswerModal = ({Report, closeModal}) => {
 
-const ReportAnswerModal = () => {
     return (
-        <div className="report-answer-modal">
-            <div className="title-div">
-                <h1>제목</h1>
-                <p>로그인이 안돼요</p>
-            </div>
+        <div className="modal-container">
+            <div className="background"></div>
+            <div className="report-answer-modal">
+                <div className="title-div">
+                    <h3>제목</h3>
+                    <p>{Report.title}</p>
+                </div>
+                <div className="writer-div">
+                    <h3>작성자 </h3>
+                    <p>{Report.name}</p>
+                </div>
+                <div className="email-div">
+                    <h3>이메일 </h3>
+                    <p>{Report.email}</p>
+                </div>
+                <div className="date-div">
+                    <h3>등록 날짜</h3>
+                    <p>{Report.email}</p>
+                </div>
+                <fieldset className="content-div">
+                    <legend>문의 내용</legend>
+                    <textarea readonly>
+                        {Report.content}
+                    </textarea>
+                </fieldset>
+                
+                <fieldset className="answer-div">
+                    <legend>답변 입력</legend>
+                    <textarea>
+                    </textarea>
+                </fieldset>
 
+                <div className="btn-frame">
+                    <Button className="answer-btn" onClick={(e)=>{closeModal()}}>답변 등록</Button>
+                    <Button className="cancel-btn" onClick={(e)=>{closeModal()}}>취소</Button>
+                </div>
+
+                </div>
         </div>
     )
 }
--- front/src/pages/ManagePage.js
+++ front/src/pages/ManagePage.js
@@ -1,4 +1,10 @@
+// TODO
+// 1. Transition.js로 페이지네이션 구현
+// 2. Reduxtookit 적용하여 상태관리
+
 /* import react*/
+import { useState } from "react";
+
 
 /* import component */
 import ManagePageHeader from "../components/Manage/ManagePageHeader";
@@ -11,22 +17,50 @@ import ReportAnswerModal from "../components/ReportManage/ReportAnswerModal"
 /* import css */
 import "../assets/css/ManagePage.css";
 
+class Report {
+  constructor(title, name, email, date, content) {
+    this.title = title;
+    this.name = name;
+    this.email = email;
+    this.date = date;
+    this.content = content;
+  }
+}
+
 const ManagePage = () => {
   const sortOptions = ["선택안함", "이메일", "이름", "이메일"];
   const filterOptions1 = ["전체", "활성", "휴면", "정지"];
   const filterOptions2 = ["전체", "처리완료", "처리접수", "등록완료"];
+
+  const [isShowModal, setIsShowMdoal] = useState(false);
   
+  
+  const report = new Report(
+    "로그인이 안돼요", "홍길동", "hong123@gmail.com", "2023.04.23.", `지금 앱 내에서 로그인 하는데,
+    구글 계정 로그인 했음에도
+    로그인이 자꾸 안됩니다...
+    
+    다시 재설치해서 해봐도
+    같은 증상 반복되는데
+    이거 어떻게 해야하나요
+    
+    지금 앱 이용하려고 하는데
+    사용조차 안되니
+    정말 답답하네요 
+    
+    빠른 처리 부탁드립니다`
+  )
 
   return (
     <>
       <div className="manage-page">
-        {/* <ManagePageHeader pageTitle={"사용자 관리"} />
+        <ManagePageHeader pageTitle={"사용자 관리"} />
         <SearchUserBar />
         <ManageSortFilterOptionBar isSort={false} filterOptions={filterOptions2} />
-        {/* 여기만 다름! */}
         {/* <UserInfoTable /> */}
-        {/* <ReportManageTable/> */}
-        <ReportAnswerModal/>        
+        <ReportManageTable showAnswerModal={()=>{setIsShowMdoal(true)}} />
+        {/* <ReportAnswerModal Report={report} />         */}
+        {isShowModal?<ReportAnswerModal Report={report} closeModal={()=>{setIsShowMdoal(false)}} />:<></>}
       </div>
     </>
   );

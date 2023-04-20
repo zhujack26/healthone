facf077c81fdb517e586c25c3afa2d2e901cf383
조원희 <onehee9710@gmail.com>
2023-04-20 16:11:28 +0900

    feat : 관리 페이지 헤더, 옵션 바, 수정 및 컴포넌트 작업 완료

--- front/src/pages/ManagePage.js
+++ front/src/pages/ManagePage.js
@@ -1,3 +1,5 @@
+/* import react*/
+
 /* import component */
 import ManagePageHeader from "../components/Manage/ManagePageHeader";
 import SearchUserBar from "../components/Manage/SearchUserBar";
@@ -9,12 +11,17 @@ import ReportManageTable from "../components/ReportManage/ReportManageTable"
 import "../assets/css/ManagePage.css";
 
 const ManagePage = () => {
+  const sortOptions = ["선택안함", "이메일", "이름", "이메일"];
+  const filterOptions1 = ["전체", "활성", "휴면", "정지"];
+  const filterOptions2 = ["전체", "처리완료", "처리접수", "등록완료"];
+  
+
   return (
     <>
       <div className="manage-page">
-        <ManagePageHeader />
+        <ManagePageHeader pageTitle={"사용자 관리"} />
         <SearchUserBar />
-        <ManageSortFilterOptionBar />
+        <ManageSortFilterOptionBar isSort={false} filterOptions={filterOptions2} />
         {/* 여기만 다름! */}
         {/* <UserInfoTable /> */}
         <ReportManageTable/>

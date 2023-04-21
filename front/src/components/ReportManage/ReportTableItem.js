d99586c07a19b961281746803d012f5f8a4bfe17
조원희 <onehee9710@gmail.com>
2023-04-21 17:57:13 +0900

    refactor : 불편사항 페이지 리팩토링

--- front/src/components/ReportManage/ReportTableItem.js
+++ front/src/components/ReportManage/ReportTableItem.js
@@ -4,17 +4,26 @@
 
 import { Button } from "@material-ui/core";
 import "../../assets/css/ReportTableItem.css";
-const ReportTableItem = ({showAnswerModal}) => {
+
+/**
+ * 
+ * @param {function} showAnswerModal 모달창 닫는 메서드
+ * @param {Object} ReportInfoData  불편사항 객체 
+ * @returns 
+ */
+const ReportTableItem = ({showAnswerModal, ReportInfoData}) => {
 
   const options = ["등록완료", "처리접수", "처리완료"];
 
   return (
     <tr className="report-table-item">
-      <td>1</td>
+
+      {Object.values(ReportInfoData).map((e, i) => <td>{e}</td>)}
+      {/* <td>1</td>
       <td>hong123@gmail.com</td>
           <td>로그인이 안돼요 </td>
           <td>2023. 04. 12.</td>
-      <td>로그인/회원가입</td>
+      <td>로그인/회원가입</td> */}
           <td>
               <select className="report-status-option">
             {options.map((e, i) => <option key={i}>{e}</option>)}

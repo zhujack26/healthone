952044912f953b022400824ac50c27c8fbfdfa6b
a0511a <a0511a@naver.com>
2023-04-23 23:57:28 +0900

    feat: 달력 및 날짜 이동구현, 칼로리조언추가

--- mobile/app/src/main/java/com/secui/healthone/ui/mealplanpage/CircularGraph.kt
+++ mobile/app/src/main/java/com/secui/healthone/ui/mealplanpage/CircularGraph.kt
@@ -1,7 +1,9 @@
 package com.secui.healthone.ui.mealplanpage
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -24,15 +26,32 @@ fun CircularGraph(intakeCalories: Int, burnedCalories: Int, totalCalories: Int, 
     val graphSize = 300.dp
     val strokeWidth = 45.dp
     val graphColors = listOf(Color(0xFFFFA726), Color(0xFFE53935))
+    val backgroundColor = Color(0xFFD3D3D3) // 옅은 회색 배경 색상
 
     Box(modifier = modifier) {
-        Canvas(modifier = Modifier
-            .size(width = graphSize, height = graphSize)
-            .offset(y = (graphSize * 0f))
+        Canvas(
+            modifier = Modifier
+                .size(width = graphSize, height = graphSize)
+                .offset(y = (graphSize * 0f))
         ) {
             val circleRadius = (size.minDimension - strokeWidth.toPx()) / 2
-            val sweepAngle = 180f * (totalCalories.toFloat() / recommendedCalories.toFloat()).coerceAtMost(1f)
 
+            // 배경용 회색 그래프 그리기
+            drawArc(
+                color = backgroundColor,
+                startAngle = 180f,
+                sweepAngle = 180f,
+                useCenter = false,
+                topLeft = Offset(
+                    (size / 2.0f).width - circleRadius,
+                    (size / 2.0f).height - circleRadius
+                ),
+                size = size.copy(width = circleRadius * 2, height = circleRadius * 2),
+                style = Stroke(width = strokeWidth.value, cap = StrokeCap.Butt)
+            )
+
+            val sweepAngle =
+                180f * (totalCalories.toFloat() / recommendedCalories.toFloat()).coerceAtMost(1f)
             val gradient = Brush.linearGradient(
                 colors = graphColors,
                 start = Offset(0f, 0f),
@@ -49,13 +68,15 @@ fun CircularGraph(intakeCalories: Int, burnedCalories: Int, totalCalories: Int, 
                     (size / 2.0f).height - circleRadius
                 ),
                 size = size.copy(width = circleRadius * 2, height = circleRadius * 2),
-                style = Stroke(width = strokeWidth.value, cap = StrokeCap.Round)
+                style = Stroke(width = strokeWidth.value, cap = StrokeCap.Butt)
             )
         }
 
         Box(
             contentAlignment = Alignment.Center,
-            modifier = Modifier.matchParentSize()
+            modifier = Modifier
+                .matchParentSize()
+                .offset(y = 40.dp) // 박스의 위치를 아래로 내립니다.
         ) {
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
@@ -63,24 +84,49 @@ fun CircularGraph(intakeCalories: Int, burnedCalories: Int, totalCalories: Int, 
             ) {
                 Text(
                     text = "총 칼로리",
-                    style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold)
+                    style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Normal)
                 )
                 Spacer(modifier = Modifier.height(4.dp))
                 Text(
                     text = "$totalCalories kcal",
                     style = TextStyle(fontSize = 24.sp, fontWeight = FontWeight.Bold)
                 )
-                Spacer(modifier = Modifier.height(40.dp))
+                Spacer(modifier = Modifier.height(20.dp))
 
                 // 섭취 칼로리 - 소모 칼로리 = 총 칼로리 텍스트
                 Text(
                     text = "섭취 칼로리 (${intakeCalories}kcal) - 소모 칼로리 (${burnedCalories}kcal)\n= 총 칼로리 (${totalCalories}kcal)",
-                    style = TextStyle(fontSize = 12.sp, fontWeight = FontWeight.Light, color = Color(0xFF827E7E)),
+                    style = TextStyle(
+                        fontSize = 12.sp,
+                        fontWeight = FontWeight.Light,
+                        color = Color(0xFF827E7E)
+                    ),
                     textAlign = TextAlign.Center // 가운데 정렬
                 )
 
                 // 총 칼로리에 따른 상태 텍스트 컴포넌트
                 CalorieStatus()
+
+                Spacer(modifier = Modifier.height(16.dp)) // 박스와 텍스트 사이의 공간을 추가합니다.
+
+                Box(
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp)
+                    .background(Color(0xFFE0E0E0), shape = RoundedCornerShape(4.dp))
+                        .padding(8.dp)
+                        .width(500.dp) // 고정 너비 설정
+                        .height(48.dp) // 고정 높이 설정
+                ) {
+                    Text(
+                        text = "권장 섭취 칼로리 2,800 ~ 3,100 \n 운동으로 200kcal만큼 소모해주세요",
+                        style = TextStyle(
+                            fontSize = 14.sp,
+                            fontWeight = FontWeight.Normal,
+                            color = Color.Black
+                        ),
+                        textAlign = TextAlign.Center // 가운데 정렬
+                    )
+                }
             }
         }
     }
--- mobile/app/src/main/java/com/secui/healthone/ui/mealplanpage/DateComponent.kt
+++ mobile/app/src/main/java/com/secui/healthone/ui/mealplanpage/DateComponent.kt
@@ -1,9 +1,78 @@
 package com.secui.healthone.ui.mealplanpage
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.Alignment
+import android.icu.util.Calendar
 
 @Composable
 fun DateComponent() {
-    Text("2023-04-20")
+    var showDialog by remember { mutableStateOf(false) }
+    val initialDate = Calendar.getInstance()
+    val selectedDate = remember { mutableStateOf(initialDate) }
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.LightGray)
+            .padding(horizontal = 8.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        // 이전 날짜로 가는 화살표
+        IconButton(onClick = {
+            selectedDate.value = (selectedDate.value.clone() as Calendar).apply { add(Calendar.DAY_OF_MONTH, -1) }
+        }) {
+            Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "Previous Date")
+        }
+
+        // 달력 아이콘과 날짜를 가운데에 배치
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            // 달력 아이콘
+            IconButton(onClick = { showDialog = true }) {
+                Icon(imageVector = Icons.Filled.DateRange, contentDescription = "Select Date")
+            }
+
+            // 선택된 날짜 표시
+            Text(
+                text = "${selectedDate.value.get(Calendar.YEAR)}.${selectedDate.value.get(Calendar.MONTH) + 1}.${selectedDate.value.get(Calendar.DAY_OF_MONTH)}",
+                textAlign = TextAlign.Center
+            )
+        }
+
+        // 다음 날짜로 가는 화살표
+        IconButton(onClick = {
+            selectedDate.value = (selectedDate.value.clone() as Calendar).apply { add(Calendar.DAY_OF_MONTH, 1) }
+        }) {
+            Icon(imageVector = Icons.Filled.ArrowForward, contentDescription = "Next Date")
+        }
+    }
+
+    DatePickerDialog(
+        showDialog = showDialog,
+        initialDate = initialDate,
+        onDateSelected = { newDate ->
+            // 날짜 선택 이벤트 처리
+            selectedDate.value = newDate
+            showDialog = false
+        },
+        onDismissRequest = {
+            showDialog = false
+        }
+    )
 }

d49dcd2d9687a6a9223634ff375574804d068372
1-Hee <onehee9710@gmail.com>
2023-04-23 13:37:55 +0900

    feat : JSON PLACEHOLDER 서버를 사용한 사용자 정보 로딩 및 렌더링 기능 구현

--- front/src/components/Manage/ManageSortFilterOptionBar.js
+++ front/src/components/Manage/ManageSortFilterOptionBar.js
@@ -4,39 +4,47 @@
 import "../../assets/css/ManageSortFilterOptionBar.css";
 
 /**
- * 
+ *
  * @param {Boolean} isSort 정렬 옵션 표시 여부
  * @param {Array} sortOptions 정렬 옵션 문자열 배열
  * @param {Boolean} isFilter 필터 옵션 표시 여부
  * @param {Array} filterOptions 필터 옵션 문자열 배열
- *  
- * @returns 
+ *
+ * @returns
  */
-const ManageSortFilterOptionBar = ({isSort=true, sortOptions=["선택안함"], isFilter=true, filterOptions=["전체"] }) => {
-
+const ManageSortFilterOptionBar = ({
+  isSort = true,
+  sortOptions = ["선택안함"],
+  isFilter = true,
+  filterOptions = ["전체"],
+}) => {
   return (
     <div className="manage-sort-filter-option-bar">
       <div className="option-div-frame">
-        {isSort ?
+        {isSort ? (
           <div className="sort-option-div">
-          <p>정렬 기준</p>
-          <select className="sort-role-select">
-            {
-              sortOptions.map((e, i) => <option>{e}</option>)
-            }
-          </select>
-        </div> : <></>
-        }
-        {isFilter ?
+            <p>정렬 기준</p>
+            <select className="sort-role-select">
+              {sortOptions.map((e, i) => (
+                <option key={i}>{e}</option>
+              ))}
+            </select>
+          </div>
+        ) : (
+          <></>
+        )}
+        {isFilter ? (
           <div className="state-filter-div">
-          <p>상태 필터</p>
-          <select className="user-state-select">
-            {
-              filterOptions.map((e, i) => <option>{e}</option>)
-            }
-          </select>
-        </div>:<></>
-        }
+            <p>상태 필터</p>
+            <select className="user-state-select">
+              {filterOptions.map((e, i) => (
+                <option key={i}>{e}</option>
+              ))}
+            </select>
+          </div>
+        ) : (
+          <></>
+        )}
       </div>
     </div>
   );
--- front/src/components/UserManage/UserTableItem.js
+++ front/src/components/UserManage/UserTableItem.js
@@ -1,3 +1,9 @@
+/* import react */
+import { useRef, useState } from "react";
+
+/* data class */
+// import UserInfo from "../../data/UserInfo.js";
+
 /* import component */
 
 /* import css */
@@ -6,36 +12,45 @@ import { Button } from "@material-ui/core";
 import "../../assets/css/UserTableItem.css";
 
 /**
- * @param {Object} UserInfoData 사용자 관리 정보 담은 객체
+ * @param {UserInfo} UserInfo 사용자 관리 정보 담은 객체
  * @returns
  */
-const UserTableItem = ({ UserInfoData }) => {
-  const options = ["선택안함", "활성", "휴면", "정지"];
+const UserTableItem = ({ UserInfo }) => {
+  const options = ["활성", "휴면", "정지"];
+
+  const selectRef = useRef();
+  const [userInfo, setUserInfo] = useState(UserInfo);
+
+  const handleStatusChange = (e) => {
+    alert("회원 상태가 수정되었습니다.");
+    const newStatus = options[selectRef.current.selectedIndex];
+    const nextUserInfo = { ...userInfo, userStatus: newStatus };
+    // 서버로 변경사항 보낼 코드
+    // TODO...
+    setUserInfo(nextUserInfo);
+  };
 
   return (
     <tr className="user-table-item">
-      {Object.values(UserInfoData).map((e, i) =>
-        i === 1 ? (
-          <td key={i}>
-            <div className="long-text">{e}</div>
-          </td>
-        ) : (
-          <td key={i}>{e}</td>
-        )
-      )}
-
       <td>
-        <select className="user-status-option">
+        <div className="long-text">{userInfo.userName}</div>
+      </td>
+      <td>
+        <div className="long-text">{userInfo.userEmail}</div>
+      </td>
+      <td>
+        <div className="long-text">{userInfo.joinDate}</div>
+      </td>
+      <td>{userInfo.userStatus}</td>
+      <td>
+        <select className="user-status-option" ref={selectRef}>
           {options.map((e, i) => (
-            <option key={i}>{e}</option>
+            <option key={i} value={e}>
+              {e}
+            </option>
           ))}
         </select>
-        <Button
-          className="apply-btn"
-          onClick={(e) => {
-            alert("변경 사항이 저장되었습니다.");
-          }}
-        >
+        <Button className="apply-btn" onClick={handleStatusChange}>
           적용
         </Button>
       </td>

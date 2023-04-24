fc510d3c3b121b8471e43b065b0b0f344055b023
박주승 <juseungpark@naver.com>
2023-04-24 11:15:52 +0900

    feat: 구글 로그인 레퍼런스 코드 추가

--- mobile/app/src/main/java/com/secui/healthone/compose/LoginPage.kt
+++ mobile/app/src/main/java/com/secui/healthone/compose/LoginPage.kt
@@ -7,7 +7,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.material.Text
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
@@ -15,9 +14,33 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.secui.healthone.R
+import android.content.Intent
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.secui.healthone.ui.login.LoginButton
 
 @Composable
 fun LoginPage() {
+    val context = LocalContext.current
+    val gso = remember {
+        GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+    }
+    val googleSignInClient = remember { GoogleSignIn.getClient(context, gso) }
+
+    val launcher = rememberLauncherForActivityResult(contract = ActivityResultContracts.StartActivityForResult()) { result ->
+        val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+        handleSignInResult(task)
+    }
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -45,13 +68,9 @@ fun LoginPage() {
         )
 
         val googleSignInPainter: Painter = painterResource(R.drawable.google_logo)
-        Image(
+        LoginButton(
             painter = googleSignInPainter,
-            contentDescription = "Google Login",
-            modifier = Modifier
-                .padding(16.dp)
-                .size(48.dp)
-//                .clickable
+            onClick = { signInWithGoogle(launcher, googleSignInClient) }
         )
 
         Text(
@@ -61,4 +80,18 @@ fun LoginPage() {
             fontSize = 12.sp
         )
     }
+}
+private fun handleSignInResult(task: Task<GoogleSignInAccount>) {
+    try {
+        val account = task.getResult(ApiException::class.java)
+    } catch (e: ApiException) {
+    }
+}
+
+private fun signInWithGoogle(
+    launcher: androidx.activity.result.ActivityResultLauncher<Intent>,
+    client: GoogleSignInClient
+) {
+    val signInIntent = client.signInIntent
+    launcher.launch(signInIntent)
 }

c8498c81aff52f24c3258a85e238accc08f4f83c
White-Asher <dkxmpower999@naver.com>
2023-04-26 03:12:51 +0900

    feat: meal 서비스 서버 프로젝트 생성

---
+++ back/healthone-meal-service/src/main/java/com/secui/healthone/HealthoneMealServiceApplication.java
@@ -0,0 +1,13 @@
+package com.secui.healthone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthoneMealServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthoneMealServiceApplication.class, args);
+	}
+
+}

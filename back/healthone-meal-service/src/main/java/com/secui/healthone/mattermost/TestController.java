a68f7f94067e1990d7f259546c2c435fe0e5a1fa
White-Asher <dkxmpower999@naver.com>
2023-04-26 14:26:37 +0900

    feat: mm설정 및 yml 설정 추가

---
+++ back/healthone-meal-service/src/main/java/com/secui/healthone/mattermost/TestController.java
@@ -0,0 +1,18 @@
+package com.secui.healthone.mattermost;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping
+    public ResponseEntity<String> test(){
+        throw new RuntimeException();
+        //return ResponseEntity.status(HttpStatus.FORBIDDEN).body("exception");
+    }
+}
+

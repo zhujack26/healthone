99424164fc27745ab3e6963a472a070b75de4855
박주승 <juseungpark@naver.com>
2023-04-27 16:47:01 +0900

    feat: 구글 소셜로그인 id토큰 발급

--- mobile/app/src/main/java/com/secui/healthone/compose/LoginPage.kt
+++ mobile/app/src/main/java/com/secui/healthone/compose/LoginPage.kt
@@ -1,5 +1,6 @@
 package com.secui.healthone.compose
 
+import android.app.Activity
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
@@ -14,6 +15,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.secui.healthone.R
 import android.content.Intent
+import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.remember
@@ -29,20 +31,35 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
 import com.secui.healthone.ui.loginpage.*
+//
+import java.io.BufferedReader
+import java.io.DataOutputStream
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import java.net.URLEncoder
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @Composable
 fun LoginPage(navController: NavController) {
     val context = LocalContext.current
     val gso = remember {
         GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(context.getString(R.string.server_client_id))
             .requestEmail()
             .build()
     }
     val googleSignInClient = remember { GoogleSignIn.getClient(context, gso) }
 
     val launcher = rememberLauncherForActivityResult(contract = ActivityResultContracts.StartActivityForResult()) { result ->
-        val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
-        handleSignInResult(task)
+        if (result.resultCode == Activity.RESULT_OK) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            handleSignInResult(navController, task)
+        } else {
+            Log.e("check", "Error1")
+        }
     }
     Column(
         modifier = Modifier
@@ -74,9 +91,7 @@ fun LoginPage(navController: NavController) {
         val googleSignInPainter: Painter = painterResource(R.drawable.login_google_logo)
         LoginButton(
             painter = googleSignInPainter,
-//            onClick = { signInWithGoogle(launcher, googleSignInClient) }
-//            나중에 구글로그인 구현할 때 사용
-            onClick = { navController.navigate("datacollect1") }
+            onClick = { signInWithGoogle(navController, launcher, googleSignInClient) }
         )
 
         Text(
@@ -88,17 +103,67 @@ fun LoginPage(navController: NavController) {
         )
     }
 }
-private fun handleSignInResult(task: Task<GoogleSignInAccount>) {
+private fun handleSignInResult(navController: NavController, task: Task<GoogleSignInAccount>) {
     try {
         val account = task.getResult(ApiException::class.java)
-    } catch (e: ApiException) {
+        val idToken = account.idToken
+        Log.d("check", "ID Token: $idToken") // ID 토큰 값 확인
+        sendIdTokenToServer(idToken)
+        navController.navigate("datacollect1")
+        Log.d("check", "check")
+    } catch (e: Exception) {
+        Log.e("check", "Error2", e)
     }
 }
 
 private fun signInWithGoogle(
+    navController: NavController,
     launcher: androidx.activity.result.ActivityResultLauncher<Intent>,
     client: GoogleSignInClient
 ) {
     val signInIntent = client.signInIntent
     launcher.launch(signInIntent)
+}
+
+private fun sendIdTokenToServer(idToken: String?) {
+    if (idToken == null) {
+        Log.e("check", "ID token is null")
+        return
+    }
+
+    CoroutineScope(Dispatchers.IO).launch {
+        try {
+            val urlString = "http://healthonetest.com"
+            val url = URL(urlString)
+            val connection = url.openConnection() as HttpURLConnection
+            connection.requestMethod = "POST"
+            Log.d("check", "check2")
+            connection.doOutput = true
+            Log.d("check", "check3")
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded")
+            Log.d("check", "check4")
+            val postData = "idToken=" + URLEncoder.encode(idToken, "UTF-8")
+
+            DataOutputStream(connection.outputStream).use { outputStream ->
+                outputStream.writeBytes(postData)
+                outputStream.flush()
+            }
+            Log.d("check", "check5")
+            val responseCode = connection.responseCode
+            Log.d("check", "Response code from server: $responseCode")
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader(InputStreamReader(connection.inputStream)).use { reader ->
+                    val response = reader.readText()
+                    Log.i("check", "Signed in as: $response")
+                }
+            } else {
+                Log.e("check", "Error sending ID token to backend. Response code: $responseCode")
+            }
+
+            connection.disconnect()
+        } catch (e: Exception) {
+            Log.e("check", "Error occurred in sendIdTokenToServer", e)
+        }
+    }
 }

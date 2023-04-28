cd05ceb0b8754108d3fbe8cd1883a3cb44eb7508
박주승 <juseungpark@naver.com>
2023-04-28 23:25:15 +0900

    feat: 설정페이지 디테일

--- mobile/app/src/main/java/com/secui/healthone/compose/SettingPage.kt
+++ mobile/app/src/main/java/com/secui/healthone/compose/SettingPage.kt
@@ -6,14 +6,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.compose.foundation.clickable
+
 
 @Composable
 fun SettingPage(navController: NavController) {
@@ -25,38 +24,38 @@ fun SettingPage(navController: NavController) {
         val textStyle = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold, color = Color.Black)
         val arrow = ">"
         val settingsItems = listOf(
-            "기본설정 및 목표 변경하기",
-            "이용약관",
-            "개인정보 보호방침",
-            "개인 데이터 다운로드",
-            "개인 데이터 삭제",
-            "로그아웃",
-            "회원탈퇴"
+            "기본설정 및 목표 변경하기" to "basic_settings",
+            "이용약관" to "terms",
+            "개인정보 보호방침" to "privacy",
+            "개인 데이터 다운로드" to "data_download",
+            "개인 데이터 삭제" to "data_deletion",
+            "로그아웃" to "logout",
+            "회원탈퇴" to "withdraw"
         )
 
-        settingsItems.forEach { item ->
-            SettingsRow(item = item, textStyle = textStyle, arrow = arrow)
+        settingsItems.forEach { (item, route) ->
+            SettingsRow(item = item, textStyle = textStyle, arrow = arrow, onClick = { navController.navigate(route) })
             Spacer(modifier = Modifier.height(16.dp))
         }
     }
 }
 
 @Composable
-fun SettingsRow(item: String, textStyle: TextStyle, arrow: String) {
+fun SettingsRow(item: String, textStyle: TextStyle, arrow: String, onClick: () -> Unit) {
     Row(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable(onClick = onClick),
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically
     ) {
         Text(
-            text = buildAnnotatedString {
-                withStyle(textStyle.toSpanStyle()) {
-                    append(item)
-                }
-                withStyle(SpanStyle(textStyle.color)) {
-                    append(arrow)
-                }
-            }
+            text = item,
+            style = textStyle
+        )
+        Text(
+            text = arrow,
+            style = textStyle
         )
     }
 }

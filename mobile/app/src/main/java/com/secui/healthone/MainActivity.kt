be8d8c72c53128b66546b73485dc2170c4e6047e
박주승 <juseungpark@naver.com>
2023-04-28 16:43:20 +0900

    feat: 수면 시간 선택 컴포넌트 구현

--- mobile/app/src/main/java/com/secui/healthone/MainActivity.kt
+++ mobile/app/src/main/java/com/secui/healthone/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
--- mobile/app/src/main/java/com/secui/healthone/compose/DataCollectPage.kt
+++ mobile/app/src/main/java/com/secui/healthone/compose/DataCollectPage.kt
@@ -1,5 +1,6 @@
 package com.secui.healthone.compose
 
+import android.content.Context
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -104,10 +105,10 @@ fun DataCollectSecondPage(navController: NavController) {
             Spacer(modifier = Modifier.height(32.dp))
             // 목표 운동량 컴포넌트
             ExcersiseAmount()
-            Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(32.dp))
             // 목표 걸음수 컴포넌트
             StepGoal()
-            Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(32.dp))
             // 목표 수면시간 컴포넌트
             SleepGoal()
             Spacer(modifier = Modifier.height(32.dp))

bd7e40e94b3cedebbbe918d5a4f07e33e383c80f
박주승 <juseungpark@naver.com>
2023-05-01 23:59:49 +0900

    feat: 걸음 목표 달성률 컴포넌트

--- mobile/app/src/main/java/com/secui/healthone/compose/WalkingPage.kt
+++ mobile/app/src/main/java/com/secui/healthone/compose/WalkingPage.kt
@@ -1,13 +1,18 @@
 package com.secui.healthone.compose
 
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import com.secui.healthone.ui.walking.LineGraph
+import com.secui.healthone.ui.walking.*
 
 @Composable
 fun WalkingPage(
     navController: NavHostController,
     modifier: Modifier=Modifier){
     LineGraph()
+    Spacer(modifier = Modifier.height(16.dp))
+    AchievementRate()
 }
--- mobile/app/src/main/java/com/secui/healthone/ui/walking/AchievementRate.kt
+++ mobile/app/src/main/java/com/secui/healthone/ui/walking/AchievementRate.kt
@@ -1,2 +1,53 @@
 package com.secui.healthone.ui.walking
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun AchievementRate(percentage: Float, onNavigate: () -> Unit) {
+    Column(modifier = Modifier.padding(16.dp)) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Text("오늘 당신의 걸음 목표 달성률은?")
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                buildAnnotatedString {
+                    withStyle(style = SpanStyle(color = Color.Blue)) {
+                        append(">")
+                    }
+                },
+                modifier = Modifier.clickable { onNavigate() }
+            )
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+        ProgressBar(percentage = percentage)
+    }
+}
+
+@Composable
+fun ProgressBar(percentage: Float) {
+    val filledWidth = (percentage * 100).toInt()
+
+    Row(
+        modifier = Modifier
+            .height(24.dp)
+            .fillMaxWidth()
+            .background(color = Color.LightGray)
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxHeight()
+                .width(filledWidth.dp)
+                .background(color = Color.Green)
+        )
+    }
+}

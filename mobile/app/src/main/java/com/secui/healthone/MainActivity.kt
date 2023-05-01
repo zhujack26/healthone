ed23bbec9ce9b098f038185fd2ee4a2b41b15a71
조원희 <onehee9710@gmail.com>
2023-05-01 13:06:05 +0900

    feat: chore : fit API 세팅 작업

--- mobile/app/src/main/java/com/secui/healthone/MainActivity.kt
+++ mobile/app/src/main/java/com/secui/healthone/MainActivity.kt
@@ -5,15 +5,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.secui.healthone.compose.*
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.fitness.Fitness
+import com.secui.healthone.compose.DataCollectFirstPage
+import com.secui.healthone.compose.DataCollectSecondPage
+import com.secui.healthone.compose.LoginPage
+import com.secui.healthone.compose.OverViewPage
 import com.secui.healthone.ui.common.TopBar
 import com.secui.healthone.util.PageRoutes
 
 
 class MainActivity : ComponentActivity() {
+
+    val APP_TAG = "SimpleHealth"
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -21,12 +32,13 @@ class MainActivity : ComponentActivity() {
             val jwtToken = sharedPreferences.getString("jwt_token", null)
 
             val navController = rememberNavController()
+            val context = LocalContext.current;
 
             NavHost(navController, startDestination = if (jwtToken != null) PageRoutes.Login.route else PageRoutes.OverView.route) {
                 // 원래 코드는 jwtToken == null
                 // if (jwtToken == null) "login" else PageRoutes.OverView.route
 
-                composable("login") {
+                composable(PageRoutes.Login.route){
                     LoginPage(navController)
                 }
                 composable(PageRoutes.OverView.route) {
@@ -35,8 +47,13 @@ class MainActivity : ComponentActivity() {
                         OverViewPage(navController)
                     }
                 }
-                composable("datacollect1") { DataCollectFirstPage(navController) }
-                composable("datacollect2") { DataCollectSecondPage(navController) }
+//                composable(PageRoutes.DataCollectFirst.route){
+//                    DataCollectFirstPage(navController)
+//                }
+//                composable(PageRoutes.DataCollectSecond.route){
+//                    DataCollectSecondPage(navController)
+//                }
+
             }
         }
     }
--- mobile/app/src/main/java/com/secui/healthone/compose/OverViewPage.kt
+++ mobile/app/src/main/java/com/secui/healthone/compose/OverViewPage.kt
@@ -1,13 +1,20 @@
 package com.secui.healthone.compose
 
+import android.util.Log
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.fitness.Fitness
+import com.secui.healthone.ui.common.TopBar
 import com.secui.healthone.ui.overviewpage.*
+import com.secui.healthone.util.PageRoutes
 
 
 @Composable
@@ -16,6 +23,23 @@ fun OverViewPage(
     modifier: Modifier = Modifier
         .fillMaxSize()
 ) {
+
+    val context = LocalContext.current;
+    val account = GoogleSignIn.getLastSignedInAccount(context)
+    if (account == null) {
+        // 사용자가 로그인하지 않았으면 로그인 화면으로 이동
+        // ...
+
+        navController.navigate(PageRoutes.Login.route)
+
+    } else {
+        // 사용자가 로그인한 경우
+        // API 클라이언트 생성
+        val historyClient = Fitness.getHistoryClient(context, account)
+         // navController.navigate(PageRoutes.OverView.route)
+        Log.d("overview:::::", "${historyClient.toString()}");
+    }
+
     Column(modifier= Modifier
         .fillMaxSize()
         .verticalScroll

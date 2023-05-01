4c6da202a8f1bd95d9caa325e6bcabd321c0316d
White-Asher <dkxmpower999@naver.com>
2023-05-01 16:26:56 +0900

    feat: dto swagger 내용 추가, Meal Swagger 추가

--- back/healthone-meal-service/src/main/java/com/secui/healthone/domain/food/dto/FoodResponseDto.java
+++ back/healthone-meal-service/src/main/java/com/secui/healthone/domain/food/dto/FoodResponseDto.java
@@ -1,17 +1,22 @@
 package com.secui.healthone.domain.food.dto;
 
 import com.secui.healthone.domain.food.entity.Food;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
+@Schema(description = "일반 음식 응답 DTO")
 public class FoodResponseDto {
-
+    @Schema(description = "사용자 음식 식별번호")
     private Integer no;
+    @Schema(description = "사용자 음식 이름")
     private String name;
+    @Schema(description = "사용자 음식 1인분당 칼로리")
     private Integer kcal;
+    @Schema(description = "사용자 음식 1인분당 그램 수")
     private Float gram;
 
     @Builder
--- back/healthone-meal-service/src/main/java/com/secui/healthone/domain/meal/dto/MealResDto.java
+++ back/healthone-meal-service/src/main/java/com/secui/healthone/domain/meal/dto/MealResDto.java
@@ -4,6 +4,7 @@ import com.secui.healthone.domain.food.dto.CustomFoodResDto;
 import com.secui.healthone.domain.food.dto.FoodResponseDto;
 import com.secui.healthone.domain.meal.entity.Meal;
 import com.secui.healthone.domain.meal.entity.MealType;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -15,18 +16,29 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 public class MealResDto {
+    @Schema(description = "식사 식별번호")
     private Integer no;
+    @Schema(description = "회원 식별번호")
+    private Integer userNo;
+    @Schema(description = "일반 음식 객체")
     private FoodResponseDto food;
+    @Schema(description = "사용자 음식 식별번호")
     private CustomFoodResDto customfood;
+    @Schema(description = "식사 기록 시간")
     private LocalDateTime createTime;
+    @Schema(description = "식사 타입 (BREAKFAST, LUNCH, DINNER, SNACK)")
     private MealType mealType;
+    @Schema(description = "섭취한 인분 수")
     private Float portion;
+    @Schema(description = "섭취한 그램 수")
     private Float gram;
+    @Schema(description = "섭취한 칼로리 수")
     private Integer kcal;
 
     @Builder
     public MealResDto(Meal entity) {
         this.no = entity.getNo();
+        this.userNo = entity.getUserNo();
         this.food = entity.getFood() != null ? new FoodResponseDto(entity.getFood()) : null;
         this.customfood = entity.getCustomfood() != null ? new CustomFoodResDto(entity.getCustomfood()) : null;
         this.createTime = entity.getCreateTime();

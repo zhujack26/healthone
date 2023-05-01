9b015945893a5e37011d9aee527b79541c1126ef
이흥종 <avjg123@naver.com>
2023-05-02 07:06:49 +0900

    feat: 최근 스트레스 불러오기

--- back/healthone-check-service/src/main/java/com/secui/healthone/domain/stress/service/impl/StressServiceImpl.java
+++ back/healthone-check-service/src/main/java/com/secui/healthone/domain/stress/service/impl/StressServiceImpl.java
@@ -11,6 +11,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -25,6 +27,29 @@ public class StressServiceImpl implements StressService {
     private StressRepository stressRepository;
 
     @Override
+    public List<Stress> getStressList(String date, int page) {
+//        String userEmail = SecurityContextHolder.getContext().getAuthentication().getName();
+//        Optional<User> optionalUser = userRepository.findByUserEmail(userEmail);
+        Optional<User> optionalUser = userRepository.findById(1);
+
+        if (optionalUser.isEmpty()) {
+        }
+        User user = optionalUser.get();
+        log.info("=========================================================================");
+        System.out.println(user.toString());
+        log.info("=========================================================================");
+        LocalDateTime localDateTime = typeConverter(date).plusNanos(1000L);
+        log.info(localDateTime.toString());
+        List<Stress> stressList = stressRepository.findAllByUserAndStressCreatetimeBeforeOrderByStressCreatetime(user, localDateTime);
+        log.info("stressList.size = " + stressList.size());
+        if(stressList.isEmpty()){
+            return null;
+        }
+        int size = stressList.size() > 7 ? 7 : stressList.size() - 1;
+        return stressList.subList(0, size);
+    }
+
+    @Override
     public void uploadUserStressInfo(UploadUserStressInfoRequestDto stressInfoDto) {
         Optional<User> optionalUser = userRepository.findById(stressInfoDto.getUserNo());
         if (optionalUser.isEmpty()) {
@@ -36,6 +61,21 @@ public class StressServiceImpl implements StressService {
                 .stressLevel(stressInfoDto.getLevel())
                 .build();
         stressRepository.save(stress);
+    }
+
+    public LocalDateTime typeConverter(String dateTime) {
+        int year = Integer.parseInt(dateTime.substring(0, 4));
+        int month = Integer.parseInt(dateTime.substring(5, 7));
+        int dayOfMonth = Integer.parseInt(dateTime.substring(8, 10));
+        if (dateTime.length() == 10) {
+            return LocalDateTime.of(year, month, dayOfMonth, 0, 0);
+        }
+        int hour = Integer.parseInt(dateTime.substring(11, 13));
+        int minute = Integer.parseInt(dateTime.substring(14, 16));
+        LocalDateTime localDateTime = LocalDateTime.of(year, month, dayOfMonth, hour, minute);
+        int second = Integer.parseInt(dateTime.substring(17, 19));
+        int nano = Integer.parseInt(dateTime.substring(20, 26)) * 1000;
+        return localDateTime.withSecond(second).withNano(nano);
     }
 
 }

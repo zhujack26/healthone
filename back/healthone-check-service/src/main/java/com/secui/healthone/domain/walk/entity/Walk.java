71216255abe6055de13e35af0cbc8ca6a1c50242
White-Asher <dkxmpower999@naver.com>
2023-05-03 01:38:41 +0900

    feat: walk domain dto, dtoMapper 구현

--- back/healthone-check-service/src/main/java/com/secui/healthone/domain/walk/entity/Walk.java
+++ back/healthone-check-service/src/main/java/com/secui/healthone/domain/walk/entity/Walk.java
@@ -1,34 +1,28 @@
 package com.secui.healthone.domain.walk.entity;
 
-import com.secui.healthone.domain.user.entity.User;
 import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
-@Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@ToString
+@Entity
+@Table(name = "sport")
 public class Walk {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int walkNo;
-
-    @ManyToOne
-    @JoinColumn(name = "user_no")
-    private User user;
-
-    @Column(nullable = false)
-    private LocalDateTime walkCreatetime;
-
-    @Column(nullable = false)
-    private int walkStepCount;
-
-    private float walkDistance;
-
+    @Column(name = "walk_no") // 걷기 식별번호
+    private Integer no;
+    @Column(name = "user_no")
+    private Integer userNo;
+    @Column(name = "walk_createtime")
+    private LocalDateTime createTime;
+    @Column(name = "walk_step_count")
+    private Integer walkStepCount;
+    @Column(name = "walk_distance")
+    private Float walkDistance;
 }

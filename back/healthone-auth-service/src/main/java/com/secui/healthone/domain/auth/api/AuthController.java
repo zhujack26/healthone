dfe1fcd9d07bc5895acd3263c76bb37040b142c8
Anhyogwan <ahg0824@naver.com>
2023-05-04 17:53:06 +0900

    [Feat] Google Login 사용자 정보 획득
    사용자 이메일 획득 성공

--- back/healthone-auth-service/src/main/java/com/secui/healthone/domain/auth/api/AuthController.java
+++ back/healthone-auth-service/src/main/java/com/secui/healthone/domain/auth/api/AuthController.java
@@ -4,8 +4,10 @@ package com.secui.healthone.domain.auth.api;
 //import com.secui.healthone.domain.auth.service.AuthService;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeTokenRequest;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
 import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.FileReader;
+import java.net.URLDecoder;
 
 @RestController
 @RequiredArgsConstructor
@@ -28,25 +31,27 @@ public class AuthController {
     public ResponseEntity test(){
         return ResponseEntity.ok().body("hello");
     }
-    @PostMapping("login")
-    public void signInOrUp(@RequestBody String authCode) {
+    @PostMapping("/login")
+    public void signInOrUp(@RequestBody String authCode) throws Exception{
         log.info("authCode = {} ",authCode);
         log.info("들어왔습니다");
         // Exchange auth code for access token
-        GoogleClientSecrets clientSecrets =
-                GoogleClientSecrets.load(
-                        JacksonFactory.getDefaultInstance(), new FileReader(CLIENT_SECRET_FILE));
-        GoogleTokenResponse tokenResponse =
-                new GoogleAuthorizationCodeTokenRequest(
-                        new NetHttpTransport(),
-                        JacksonFactory.getDefaultInstance(),
-                        "https://oauth2.googleapis.com/token",
-                        clientSecrets.getDetails().getClientId(),
-                        clientSecrets.getDetails().getClientSecret(),
-                        authCode,
-                        REDIRECT_URI)  // Specify the same redirect URI that you use with your web
-                        // app. If you don't have a web version of your app, you can
-                        // specify an empty string.
-                        .execute();
+        JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+        GoogleAuthorizationCodeTokenRequest tokenRequest = new GoogleAuthorizationCodeTokenRequest(
+                new NetHttpTransport(),
+                jsonFactory,
+                "https://oauth2.googleapis.com/token",
+                "785216230516-1h8hoagkginolldjirvtgiebidmqe38i.apps.googleusercontent.com",
+                "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",
+                URLDecoder.decode(authCode),
+                "http://localhost" // This should match the redirect_uri in your Android app
+        );
+        GoogleTokenResponse tokenResponse = tokenRequest.execute();
+        GoogleIdToken idToken = tokenResponse.parseIdToken();
+        GoogleIdToken.Payload payload = idToken.getPayload();
+        String userId = payload.getSubject();
+        log.info("IDTOKEN = {}",userId);
+        log.info("EMail= {}",payload.getEmail());
+
     }
 }
--- back/healthone-auth-service/src/main/java/com/secui/healthone/domain/auth/cofngi/SecurityConfig.java
+++ back/healthone-auth-service/src/main/java/com/secui/healthone/domain/auth/cofngi/SecurityConfig.java
@@ -1,59 +1,59 @@
-package com.secui.healthone.domain.auth.cofngi;
-
-import com.secui.healthone.domain.auth.service.CustomOAuth2UserService;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
-
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-@Slf4j
-@Configuration
-@EnableWebSecurity
-@RequiredArgsConstructor
-public class SecurityConfig {
-
-    private final CustomOAuth2UserService customOAuth2UserService;
-//    private final OAuth2SuccessHandler oAuth2SuccessHandler;
-//    private final TokenService tokenservice;
-//    private final CustomLogoutSuccessHandler logoutSuccessHandler;
-//    private final RedisUtil redisUtil;
-//    private final RedisRepository redisRepository;
-//    private final CookieUtil cookieUtil;
+//package com.secui.healthone.domain.auth.cofngi;
 //
-//    private final OAuth2FailureHandler oAuth2FailureHandler;
-
-    @Bean
-    protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http
-                .httpBasic().disable()
-                .cors().and()
-                .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authorizeRequests()
-//                .antMatchers("/user/**").authenticated()
-                .anyRequest().permitAll()
-                .and()
-                .logout()
-//                .logoutUrl("/user/logout")
-//                .logoutSuccessHandler(logoutSuccessHandler)
-                .logoutSuccessUrl("/")
-                .and()
-//                .addFilterBefore(new JwtAuthenticationFilter(tokenservice, redisUtil, cookieUtil, redisRepository), UsernamePasswordAuthenticationFilter.class)
-                .oauth2Login()
-//                .successHandler(oAuth2SuccessHandler)
-//                .failureHandler(oAuth2FailureHandler)
-                .userInfoEndpoint()
-                .userService(customOAuth2UserService);
-
-        return http.build();
-
-    }
-}
+//import com.secui.healthone.domain.auth.service.CustomOAuth2UserService;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//
+//@Slf4j
+//@Configuration
+//@EnableWebSecurity
+//@RequiredArgsConstructor
+//public class SecurityConfig {
+//
+//    private final CustomOAuth2UserService customOAuth2UserService;
+////    private final OAuth2SuccessHandler oAuth2SuccessHandler;
+////    private final TokenService tokenservice;
+////    private final CustomLogoutSuccessHandler logoutSuccessHandler;
+////    private final RedisUtil redisUtil;
+////    private final RedisRepository redisRepository;
+////    private final CookieUtil cookieUtil;
+////
+////    private final OAuth2FailureHandler oAuth2FailureHandler;
+//
+//    @Bean
+//    protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+//        http
+//                .httpBasic().disable()
+//                .cors().and()
+//                .csrf().disable()
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and()
+//                .authorizeRequests()
+////                .antMatchers("/user/**").authenticated()
+//                .anyRequest().permitAll()
+//                .and()
+//                .logout()
+////                .logoutUrl("/user/logout")
+////                .logoutSuccessHandler(logoutSuccessHandler)
+//                .logoutSuccessUrl("/")
+//                .and()
+////                .addFilterBefore(new JwtAuthenticationFilter(tokenservice, redisUtil, cookieUtil, redisRepository), UsernamePasswordAuthenticationFilter.class)
+//                .oauth2Login()
+////                .successHandler(oAuth2SuccessHandler)
+////                .failureHandler(oAuth2FailureHandler)
+//                .userInfoEndpoint()
+//                .userService(customOAuth2UserService);
+//
+//        return http.build();
+//
+//    }
+//}

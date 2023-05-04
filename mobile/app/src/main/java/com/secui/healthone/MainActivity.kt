b171408482ef609b06dee73e4163b89cd2ef37b1
조원희 <onehee9710@gmail.com>
2023-05-04 17:58:38 +0900

    chore : 임시 업로드

--- mobile/app/src/main/java/com/secui/healthone/MainActivity.kt
+++ mobile/app/src/main/java/com/secui/healthone/MainActivity.kt
@@ -3,18 +3,12 @@ package com.secui.healthone
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
+import android.os.SystemClock
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.lifecycle.Observer
+import androidx.core.content.ContextCompat
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -25,15 +19,18 @@ import com.secui.healthone.compose.OverViewPage
 import com.secui.healthone.ui.common.TopBar
 import com.secui.healthone.util.PageRoutes
 import com.secui.healthone.compose.*
-import com.secui.healthone.repository.HealthOneRepository
-import com.secui.healthone.service.ScreenService
-import com.secui.healthone.viewmodel.HealthOneViewModel
-import java.time.LocalDate
-import java.time.LocalDateTime
-import java.util.Date
+import com.secui.healthone.service.MyService
+import com.secui.healthone.util.PreferenceUtil
 
 
 class MainActivity : ComponentActivity() {
+
+    lateinit var prefs:PreferenceUtil;
+
+    override fun onResume() {
+        super.onResume()
+        prefs = PreferenceUtil(this);
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -78,12 +75,16 @@ class MainActivity : ComponentActivity() {
 
     override fun onStart() {
         super.onStart()
-        // startService(Intent(this, ScreenService::class.java))
     }
 
     // Activity가 화면에서 사라질 때 호출
     override fun onStop() {
         super.onStop()
+        // SystemClock.elapsedRealtime();
+        // 서비스 시작
+//        val serviceIntent = Intent(this, MyService::class.java)
+//        // ContextCompat.startForegroundService(this, serviceIntent)
+//        startService(serviceIntent);
     }
 
 

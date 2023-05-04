ed72e101dd4af1e12baff6a759a81ad9370870bc
White-Asher <dkxmpower999@naver.com>
2023-05-04 14:17:34 +0900

    chore: stress 주석처리

--- back/healthone-check-service/src/main/java/com/secui/healthone/domain/stress/dto/UploadUserStressInfoRequestDto.java
+++ back/healthone-check-service/src/main/java/com/secui/healthone/domain/stress/dto/UploadUserStressInfoRequestDto.java
@@ -1,18 +1,18 @@
-package com.secui.healthone.domain.stress.dto;
-
-import lombok.Getter;
-import lombok.Setter;
-
-import java.time.LocalDateTime;
-
-@Getter
-@Setter
-public class UploadUserStressInfoRequestDto {
-
-    private int userNo;
-
-    private int level;
-
-    private LocalDateTime createTime;
-
-}
+//package com.secui.healthone.domain.stress.dto;
+//
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//import java.time.LocalDateTime;
+//
+//@Getter
+//@Setter
+//public class UploadUserStressInfoRequestDto {
+//
+//    private int userNo;
+//
+//    private int level;
+//
+//    private LocalDateTime createTime;
+//
+//}

5889401375dbdbdf9b1df1a1d8acedfe360992be
White-Asher <dkxmpower999@naver.com>
2023-05-04 16:59:24 +0900

    fix: SportRecord entity, dto 불필요한 필드 삭제 및 매핑 제거

--- back/healthone-meal-service/src/main/java/com/secui/healthone/domain/sportrecord/dto/SportRecordDtoMapper.java
+++ back/healthone-meal-service/src/main/java/com/secui/healthone/domain/sportrecord/dto/SportRecordDtoMapper.java
@@ -1,7 +1,5 @@
 package com.secui.healthone.domain.sportrecord.dto;
 
-import com.secui.healthone.domain.sport.entity.CustomSport;
-import com.secui.healthone.domain.sport.entity.Sport;
 import com.secui.healthone.domain.sportrecord.entity.SportRecord;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -15,16 +13,16 @@ public interface SportRecordDtoMapper {
 
     SportRecordDtoMapper INSTANCE = Mappers.getMapper(SportRecordDtoMapper.class);
 
-    @Mapping(source = "sportRecord.sport", target="sportResDto")
-    @Mapping(source = "sportRecord.customSport", target="customSportResDto")
+//    @Mapping(source = "sportRecord.sport", target="sportResDto")
+//    @Mapping(source = "sportRecord.customSport", target="customSportResDto")
     SportRecordResDto entityToResDto(SportRecord sportRecord);
 
-    @Mapping(source = "sportRecord.sport", target="sportResDto")
-    @Mapping(source = "sportRecord.customSport", target="customSportResDto")
+//    @Mapping(source = "sportRecord.sport", target="sportResDto")
+//    @Mapping(source = "sportRecord.customSport", target="customSportResDto")
     List<SportRecordResDto> entityToResListDto(List<SportRecord> sportRecords);
 
     @Mapping(source = "sportRecordReqDto.no", target="no")
     @Mapping(source = "sportRecordReqDto.userNo", target="userNo")
-    SportRecord reqDtoToEntity(SportRecordReqDto sportRecordReqDto, Sport sport, CustomSport customSport);
+    SportRecord reqDtoToEntity(SportRecordReqDto sportRecordReqDto);
 
 }

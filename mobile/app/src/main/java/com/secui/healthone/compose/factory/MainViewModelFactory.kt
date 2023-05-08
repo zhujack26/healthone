fec2ff106c900ecd453178adaf3a04b98631d01f
조원희 <onehee9710@gmail.com>
2023-05-09 08:44:46 +0900

    feat : 심박수 rest 기눙 추가

--- mobile/app/src/main/java/com/secui/healthone/compose/factory/MainViewModelFactory.kt
+++ mobile/app/src/main/java/com/secui/healthone/compose/factory/MainViewModelFactory.kt
@@ -1,14 +1,11 @@
 package com.secui.healthone.compose.factory
 
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.secui.healthone.repository.HealthOneRepository
-import com.secui.healthone.viewmodel.HealthOneViewModel
 
 class MainViewModelFactory(
-    private val repository : HealthOneRepository
+    // private val repository : HealthOneRepository
 ) : ViewModelProvider.Factory {
-    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return HealthOneViewModel(repository) as T
-    }
+//    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+//        return HealthOneViewModel(repository) as T
+//    }
 }
--- mobile/app/src/main/java/com/secui/healthone/ui/heart/heartratepage/HeartRateRecordItem.kt
+++ mobile/app/src/main/java/com/secui/healthone/ui/heart/heartratepage/HeartRateRecordItem.kt
@@ -1,5 +1,6 @@
 package com.secui.healthone.ui.heart.heartratepage
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -8,15 +9,20 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.secui.healthone.R;
+import com.secui.healthone.data.heart.HeartRead
 import com.secui.healthone.ui.common.AppColors
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 @Composable
 fun HeartRateRecordItem(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    heartRate: HeartRead
 ){
     Card(
         elevation = 4.dp,
@@ -29,78 +35,60 @@ fun HeartRateRecordItem(
             .wrapContentHeight()
             .padding(8.dp),
             verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+            horizontalAlignment = Alignment.End
         ) {
 
-            // 측정 값 + 측정 날짜
             Row(modifier = Modifier
                 .fillMaxWidth()
                 .wrapContentHeight()
-                .padding(16.dp, 8.dp),
-                verticalAlignment = Alignment.CenterVertically,
+                .padding(16.dp),
+                verticalAlignment = Alignment.Bottom,
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
 
+                // 날짜
+                Text(
+                    text = getParsingDateString(heartRate.createTime),
+                    fontSize = 14.sp,
+                    color = AppColors.mono700
+                )
+
+                // 심박수
                 Row(modifier = Modifier
                     .wrapContentWidth()
                     .wrapContentHeight(),
                     verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.End
+                    horizontalArrangement = Arrangement.SpaceBetween
                 ) {
-
-                    Text(text = HeartRateRecordItemText.heartBpmValue,
-                        fontSize = 16.sp,
-                        color = AppColors.mono900
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_heart),
+                        contentDescription = "하트",
+                        modifier = Modifier
+                            .width(32.dp)
+                            .height(32.dp),
+                        contentScale = ContentScale.Fit
                     )
-                    Spacer(modifier = Modifier.width(4.dp))
-                    Text(text = HeartRateRecordItemText.heartBpmUnit,
-                        fontSize = 16.sp,
-                        color = AppColors.mono900
-                    )
+                    Spacer(modifier = Modifier.width(16.dp));
+                    Row(modifier= Modifier
+                        .wrapContentWidth()
+                        .wrapContentHeight(),
+                        verticalAlignment = Alignment.Bottom
+                    ) {
+                        Text(text = heartRate.count.toString(), fontSize = 28.sp);
+                        Spacer(modifier = Modifier.width((8.dp)));
+                        Text(text = "bpm", fontSize = 16.sp);
+                    }
                 }
-
-                Text(text = HeartRateRecordItemText.heartBpmDate,
-                    fontSize = 16.sp,
-                    color = AppColors.mono900
-                )
-
-            }
-
-            Spacer(modifier = Modifier.height(16.dp));
-
-            // 측정 장비 정보 + 측정 시간
-            Row(modifier = Modifier
-                .fillMaxWidth()
-                .wrapContentHeight()
-                .padding(16.dp, 8.dp),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-
-                Text(text = HeartRateRecordItemText.heartBpmTime,
-                    fontSize = 16.sp,
-                    color = AppColors.mono900
-                )
-                Text(text = HeartRateRecordItemText.wearableDeviceName,
-                    fontSize = 16.sp,
-                    color = AppColors.mono900
-                )
-
             }
 
         }
-
     }
-
 }
 
-class HeartRateRecordItemText {
-    companion object {
-        const val heartBpmValue = "82"
-        const val heartBpmUnit = "bpm"
-        const val heartBpmDate = "2023년 4월 10일"
-        const val heartBpmTime = "오후 06:10 "
-        const val wearableDeviceName = "Galaxy Watch4 Classic"
 
-    }
+fun getParsingDateString(inputString:String):String{
+    val inputFormatter = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.getDefault())
+    val outputFormatter = SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault())
+    val date = inputFormatter.parse(inputString)
+    return outputFormatter.format(date)
 }

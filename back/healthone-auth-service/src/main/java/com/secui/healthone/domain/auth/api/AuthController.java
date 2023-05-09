140c207d90aaf1af214d6717e8b9fb2de4befda8
Anhyogwan <ahg0824@naver.com>
2023-05-10 01:12:40 +0900

    [Feat] Token 관련 추가작업
    쿠키에 리프레쉬담고, 헤더에 엑세스토큰 담아서 리턴 하는 방식 구현

--- back/healthone-auth-service/src/main/java/com/secui/healthone/domain/auth/api/AuthController.java
+++ back/healthone-auth-service/src/main/java/com/secui/healthone/domain/auth/api/AuthController.java
@@ -9,10 +9,14 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
+import com.secui.healthone.CookieUtil;
 import com.secui.healthone.domain.auth.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.methods.HttpHead;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 //import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
@@ -21,9 +25,12 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.FileReader;
+import java.io.IOException;
 import java.net.URLDecoder;
 import java.util.HashMap;
 import java.util.Map;
+
+import static org.springframework.http.HttpHeaders.SET_COOKIE;
 
 @RestController
 @RequiredArgsConstructor
@@ -33,16 +40,26 @@ public class AuthController {
 
     private final AuthService authService;
 
+    private final CookieUtil cookieUtil;
+
     @PostMapping("/login")
-    public ResponseEntity<Map<String,String>> signInOrUp(@RequestBody String authCode) throws Exception{
+    public ResponseEntity signInOrUp(@RequestBody String authCode){
         log.info("authCode = {} ",authCode);
         try {
             Map<String, String> token = authService.isSignUp(authCode);
             log.info("Token generated = {} ",token.entrySet());
-            return ResponseEntity.ok().body(token);
+//            response.addHeader("Authorization",token.get("accesstoken"));
+//            response.setHeader("Set-Cookie",cookieUtil.getCookie(token.get("refreshtoken"),1000L * 60L * 60L * 24L * 30L));
+            return ResponseEntity.ok()
+                    .header(SET_COOKIE,cookieUtil.getCookie(token.get("refreshtoken"),1000L * 60L * 60L * 24L * 30L).toString())
+                    .header("Authorization",token.get("accesstoken"))
+                    .build();
+        }catch (IOException e){
+            e.getStackTrace();
+            return ResponseEntity.badRequest().build();
         }catch (Exception e){
             e.getStackTrace();
-            return ResponseEntity.badRequest().body(null);
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
     }
 

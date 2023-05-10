9ab2789aac8f09c34cd76e4a89f4b850bfc875fc
White-Asher <dkxmpower999@naver.com>
2023-05-10 22:46:50 +0900

    rest: 회원 헬스정보 controller swagger정의

--- back/healthone-info-service/src/main/java/com/secui/healthone/domain/healthInfo/api/HealthInfoController.java
+++ back/healthone-info-service/src/main/java/com/secui/healthone/domain/healthInfo/api/HealthInfoController.java
@@ -1,10 +1,18 @@
 package com.secui.healthone.domain.healthInfo.api;
 
+import com.secui.healthone.domain.healthAdvice.dto.HealthAdviceDto;
 import com.secui.healthone.domain.healthInfo.dto.GetHealthInfoReqDto;
 import com.secui.healthone.domain.healthInfo.dto.HealthInfoDeleteReqDto;
 import com.secui.healthone.domain.healthInfo.dto.HealthInfoDto;
 import com.secui.healthone.domain.healthInfo.service.HealthInfoService;
 import com.secui.healthone.global.response.RestApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -13,27 +21,49 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @Slf4j
 @RequestMapping("/api/health-info")
+@Tag(name = "HealthInfo", description = "회원 헬스 정보 관련 컨트롤러")
 public class HealthInfoController {
 
     private final HealthInfoService healthInfoService;
 
+    @Operation(summary = "회원 헬스 정보 조회", description = "회원 헬스 정보 조회 API", tags = {"HealthInfo"})
+    @ApiResponses({@ApiResponse(responseCode = "200", description = "회원 헬스 정보 조회 성공", content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = HealthInfoDto.class)),
+            @Content(mediaType = "*/*", schema = @Schema(implementation = RestApiResponse.class)) }), })
+    @SecurityRequirement(name = "bearerAuth")
     @GetMapping
     public RestApiResponse<HealthInfoDto> getHealthInfo(@RequestBody GetHealthInfoReqDto getHealthInfoReqDto) {
         HealthInfoDto healthInfoDto = healthInfoService.getHealthInfo(getHealthInfoReqDto);
-        return new RestApiResponse<>("", healthInfoDto);
+        return new RestApiResponse<>("회원 헬스 정보 조회 성공", healthInfoDto);
     }
+
+    @Operation(summary = "회원 헬스 정보 등록", description = "회원 헬스 정보 등록 API", tags = {"HealthInfo"})
+    @ApiResponses({@ApiResponse(responseCode = "200", description = "회원 헬스 데이터 등록 성공", content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = HealthInfoDto.class)),
+            @Content(mediaType = "*/*", schema = @Schema(implementation = RestApiResponse.class)) }), })
+    @SecurityRequirement(name = "bearerAuth")
     @PostMapping
     public RestApiResponse<?> addHealthInfo(@RequestBody HealthInfoDto healthInfoDto) {
         healthInfoService.addHealthInfo(healthInfoDto);
         return new RestApiResponse<>("회원 헬스 데이터 등록 성공", null);
     }
 
+    @Operation(summary = "회원 헬스 정보 수정", description = "회원 헬스 정보 수정 API", tags = {"HealthInfo"})
+    @ApiResponses({@ApiResponse(responseCode = "200", description = "회원 헬스 데이터 수정 성공", content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = HealthInfoDto.class)),
+            @Content(mediaType = "*/*", schema = @Schema(implementation = RestApiResponse.class)) }), })
+    @SecurityRequirement(name = "bearerAuth")
     @PatchMapping
     public RestApiResponse<?> updateHealthInfo(@RequestBody HealthInfoDto healthInfoDto) {
         healthInfoService.updateHealthInfo(healthInfoDto);
         return new RestApiResponse<>("회원 헬스 데이터 수정 성공", null);
     }
 
+    @Operation(summary = "회원 헬스 정보 삭제", description = "회원 헬스 정보 삭제 API", tags = {"HealthInfo"})
+    @ApiResponses({@ApiResponse(responseCode = "200", description = "회원 헬스 데이터 수정 성공", content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = HealthInfoDto.class)),
+            @Content(mediaType = "*/*", schema = @Schema(implementation = RestApiResponse.class)) }), })
+    @SecurityRequirement(name = "bearerAuth")
     @DeleteMapping
     public RestApiResponse<?> deleteHealthInfo(@RequestParam String no) {
         healthInfoService.deleteHealthInfo(no);

43bf8c84519736128370f96b0885f7de00687db1
White-Asher <dkxmpower999@naver.com>
2023-05-11 15:38:40 +0900

    fix: sportrecord 수정 API 버그 수정, 유효성 검증 추가

--- back/healthone-meal-service/src/main/java/com/secui/healthone/domain/sportrecord/dto/SportRecordDtoMapper.java
+++ back/healthone-meal-service/src/main/java/com/secui/healthone/domain/sportrecord/dto/SportRecordDtoMapper.java
@@ -13,14 +13,8 @@ public interface SportRecordDtoMapper {
 
     SportRecordDtoMapper INSTANCE = Mappers.getMapper(SportRecordDtoMapper.class);
 
-//    @Mapping(source = "sportRecord.sport", target="sportResDto")
-//    @Mapping(source = "sportRecord.customSport", target="customSportResDto")
     SportRecordResDto entityToResDto(SportRecord sportRecord);
-
-//    @Mapping(source = "sportRecord.sport", target="sportResDto")
-//    @Mapping(source = "sportRecord.customSport", target="customSportResDto")
     List<SportRecordResDto> entityToResListDto(List<SportRecord> sportRecords);
-
     @Mapping(source = "sportRecordReqDto.no", target="no")
     @Mapping(source = "sportRecordReqDto.userNo", target="userNo")
     SportRecord reqDtoToEntity(SportRecordReqDto sportRecordReqDto);

429cc9472a2205e46ac5f29b75a1e3252f255c5f
White-Asher <dkxmpower999@naver.com>
2023-05-11 14:23:22 +0900

    fix: 음식 반환 dto 불필요한 valid 제거

--- back/healthone-meal-service/src/main/java/com/secui/healthone/domain/food/dto/FoodResponseDto.java
+++ back/healthone-meal-service/src/main/java/com/secui/healthone/domain/food/dto/FoodResponseDto.java
@@ -13,16 +13,12 @@ import javax.validation.constraints.NotNull;
 @Schema(description = "일반 음식 응답 DTO")
 public class FoodResponseDto {
     @Schema(description = "음식 식별번호")
-    @NotNull(message = "음식 식별번호는 null 일 수 없습니다.")
     private Integer no;
     @Schema(description = "음식 이름")
-    @NotNull(message = "음식 이름은 null 일 수 없습니다.")
     private String name;
     @Schema(description = "음식 1인분당 칼로리")
-    @NotNull(message = "음식 1인분당 칼로리 null 일 수 없습니다.")
     private Integer kcal;
     @Schema(description = "음식 1인분당 그램 수")
-    @NotNull(message = "음식 1인분당 그램 수는 null 일 수 없습니다.")
     private Float gram;
 
     @Builder

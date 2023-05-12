cb585b13d6070765cf11ff89ce4b76fe7775d3d1
White-Asher <dkxmpower999@naver.com>
2023-05-13 01:47:04 +0900

    feat: Walk(걷기) 도메인 TOKEN 적용

--- back/healthone-check-service/src/main/java/com/secui/healthone/domain/walk/repository/WalkRepository.java
+++ back/healthone-check-service/src/main/java/com/secui/healthone/domain/walk/repository/WalkRepository.java
@@ -5,15 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface WalkRepository extends JpaRepository<Walk, Integer> {
 
-    List<Walk> findAllByUserNoAndCreatetimeBetween(int user, LocalDateTime createtime1, LocalDateTime createtime2);
-//    List<Walk> findAllByUserAndWalkCreatetimeBetween(User user, LocalDateTime walkCreatetime1, LocalDateTime walkCreatetime2);
-
-    List<Walk> findAllByUserNoAndCreatetime(int userNo, LocalDateTime createtime);
-//    List<Walk> findAllByUserAndWalkCreatetime(User user, LocalDateTime walkCreatetime);
-
+    List<Walk> findAllByUserNoAndCreatetimeBetween(Integer user, LocalDateTime createtime1, LocalDateTime createtime2);
+    List<Walk> findAllByUserNoAndCreatetime(Integer userNo, LocalDateTime createtime);
+    Optional<Walk> findByNoAndUserNo(Integer no, Integer userNo);
     Walk save(Walk walk);
-
+    void deleteByNoAndUserNo(Integer no, Integer userNo);
 }
--- back/healthone-check-service/src/main/java/com/secui/healthone/domain/walk/service/WalkServiceImpl.java
+++ back/healthone-check-service/src/main/java/com/secui/healthone/domain/walk/service/WalkServiceImpl.java
@@ -21,25 +21,17 @@ public class WalkServiceImpl implements WalkService {
     private final WalkRepository walkRepository;
 
     @Override
-    public List<WalkResDto> getWalkEntitiesForSevenDays(String dateTime) {
-//        Optional<User> optionalUser = userRepository.findById(1);
-//        String userEmail = SecurityContextHolder.getContext().getAuthentication().getName();
-//        Optional<User> optionalUser = userRepository.findByUserEmail(userEmail);
-//        User user = optionalUser.orElseThrow(()-> new RestApiException(CustomErrorCode.DB_100));
+    public List<WalkResDto> getWalkEntitiesForSevenDays(String dateTime, Integer userNo) {
         LocalDateTime endDateTime = typeConverter(dateTime);
         LocalDateTime startDateTime = endDateTime.minusDays(6).withHour(0).withMinute(0).withSecond(0).withNano(0);
-        List<Walk> walkList = walkRepository.findAllByUserNoAndCreatetimeBetween(1, startDateTime, endDateTime);
+        List<Walk> walkList = walkRepository.findAllByUserNoAndCreatetimeBetween(userNo, startDateTime, endDateTime);
         return WalkDtoMapper.INSTANCE.entityToResDto(walkList);
     }
 
     @Override
-    public List<WalkResDto> getDetailedWalkInfo(String date) {
-//        Optional<User> optionalUser = userRepository.findById(1);
-//        String userEmail = SecurityContextHolder.getContext().getAuthentication().getName();
-//        Optional<User> optionalUser = userRepository.findByUserEmail(userEmail);
-//        User user = optionalUser.orElseThrow(()-> new RestApiException(CustomErrorCode.DB_100));
+    public List<WalkResDto> getDetailedWalkInfo(String date, Integer userNo) {
         LocalDateTime localDateTime = typeConverter(date);
-        List<Walk> walkList = walkRepository.findAllByUserNoAndCreatetime(1, localDateTime);
+        List<Walk> walkList = walkRepository.findAllByUserNoAndCreatetime(userNo, localDateTime);
         return WalkDtoMapper.INSTANCE.entityToResDto(walkList);
     }
 
@@ -51,9 +43,9 @@ public class WalkServiceImpl implements WalkService {
     }
 
     @Override
-    public void deleteWalk(Integer no) {
-        Walk getWalk = walkRepository.findById(no).orElseThrow(()-> new RestApiException(CustomErrorCode.DB_100));
-        walkRepository.delete(getWalk);
+    public void deleteWalk(Integer no, Integer userNo) {
+        walkRepository.findByNoAndUserNo(no, userNo).orElseThrow(()-> new RestApiException(CustomErrorCode.DB_100));
+        walkRepository.deleteByNoAndUserNo(no, userNo);
     }
 
 

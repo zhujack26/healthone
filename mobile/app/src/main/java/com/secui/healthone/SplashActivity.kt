5a8fb116fdb657fd62f674a10b6fba516c4e6ab4
조원희 <onehee9710@gmail.com>
2023-05-12 17:08:30 +0900

    style : top bar 스타일 변경

--- mobile/app/src/main/java/com/secui/healthone/SplashActivity.kt
+++ mobile/app/src/main/java/com/secui/healthone/SplashActivity.kt
@@ -3,20 +3,29 @@ package com.secui.healthone
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.lifecycle.lifecycleScope
+import com.secui.healthone.service.ScreenService
 import kotlinx.coroutines.delay
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity: ComponentActivity() {
 
+    override fun onResume() {
+        super.onResume()
+        // 화면 on/off 감지를 위한 Service 시작
+        Log.i("SPLASH ::: ", "수면측정 기능 on...");
+        val serviceIntent = Intent(this, ScreenService::class.java)
+        startForegroundService(serviceIntent);
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash);
 
         lifecycleScope.launchWhenCreated {
-            delay(1000)
 
+            delay(1000)
             val intent = Intent(this@SplashActivity, MainActivity::class.java)
             startActivity(intent)
             finish()

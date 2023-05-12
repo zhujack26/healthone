06935ab52cb5388e7d83e6e25f38a21fb3ef04d2
White-Asher <dkxmpower999@naver.com>
2023-05-12 15:50:10 +0900

    feat: Calorie 정보 7일치 반환 API 구현

--- back/healthone-meal-service/src/main/java/com/secui/healthone/domain/calorie/service/CalorieService.java
+++ back/healthone-meal-service/src/main/java/com/secui/healthone/domain/calorie/service/CalorieService.java
@@ -5,24 +5,31 @@ import com.secui.healthone.domain.meal.entity.Meal;
 import com.secui.healthone.domain.meal.repository.MealRepository;
 import com.secui.healthone.domain.sportrecord.entity.SportRecord;
 import com.secui.healthone.domain.sportrecord.repository.SportRecordRepository;
+import com.secui.healthone.global.error.errorcode.CommonErrorCode;
+import com.secui.healthone.global.error.exception.RestApiException;
 import com.secui.healthone.global.util.StringDateTrans;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+import static com.secui.healthone.global.util.StringDateTrans.stringDateTrans7Days;
+
 @Service
 @RequiredArgsConstructor
 public class CalorieService {
     private final SportRecordRepository sportRecordRepository;
     private final MealRepository mealRepository;
-//    private final CalorieRepository calorieRepository;
-//    private final CalorieDtoMapper calorieDtoMapper;
-
-    public CalorieResDto getCalorie(String date, Integer userNo) {
-        StringDateTrans transDate = new StringDateTrans(date);
-
-        List<Meal> getMeal = mealRepository.findByCreateTimeBetweenAndUserNo(transDate.getStartDateTime(), transDate.getEndDateTime(), 1);
+    public CalorieResDto getCalorie(String date, Integer userNo, String mode) {
+        StringDateTrans transDate = null;
+        if(mode.equals("day")) {
+            transDate = new StringDateTrans(date);
+        } else if (mode.equals("week")) {
+            transDate = stringDateTrans7Days(date);
+        } else {
+            throw new RestApiException(CommonErrorCode.INTERNAL_SERVER_ERROR);
+        }
+        List<Meal> getMeal = mealRepository.findByCreateTimeBetweenAndUserNo(transDate.getStartDateTime(), transDate.getEndDateTime(), userNo);
         Integer totalEatenKcal = 0;
         for (Meal meal : getMeal) {
             totalEatenKcal += meal.getKcal();
@@ -32,7 +39,6 @@ public class CalorieService {
         for (SportRecord sportRecord : getSportRecord) {
             totalConsumeKcal += sportRecord.getConsumeCalorie();
         }
-
         return CalorieResDto.builder()
                 .userNo(userNo)
                 .sumKcalEaten(totalEatenKcal)

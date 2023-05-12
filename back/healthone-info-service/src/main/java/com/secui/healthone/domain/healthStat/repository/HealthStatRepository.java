fd04ef51295bd9bc8f959ea6c34d9057322ef943
White-Asher <dkxmpower999@naver.com>
2023-05-12 22:26:21 +0900

    fix: 건강 조언 최신 데이터 기준으로 출력

--- back/healthone-info-service/src/main/java/com/secui/healthone/domain/healthStat/repository/HealthStatRepository.java
+++ back/healthone-info-service/src/main/java/com/secui/healthone/domain/healthStat/repository/HealthStatRepository.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 
 public interface HealthStatRepository extends JpaRepository<HealthStat, Integer> {
     Optional<HealthStat> findByUserNoAndCreateTimeBetween(Integer no, LocalDateTime startDateTime, LocalDateTime endDateTime);
+    Optional<HealthStat> findTopByUserNoOrderByCreateTimeDesc(Integer userNo);
     HealthStat save (HealthStat healthStat);
     void deleteByNoAndUserNo(Integer no, Integer userNo);
 }

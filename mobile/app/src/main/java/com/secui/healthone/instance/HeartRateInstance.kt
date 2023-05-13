3e6584bb11db9538739d2d960ef72e366d7faf2c
1-Hee <onehee9710@gmail.com>
2023-05-13 12:24:01 +0900

    chore : 토큰 적재시 판별을 위해 값 출력로그 추가

--- mobile/app/src/main/java/com/secui/healthone/instance/HeartRateInstance.kt
+++ mobile/app/src/main/java/com/secui/healthone/instance/HeartRateInstance.kt
@@ -32,6 +32,7 @@ class HeartRateInstance : AppCompatActivity() {
             override fun loadForRequest(url: HttpUrl): List<Cookie> {
                 val cookies = cookieStore[url.host] ?: return emptyList()
                 val refreshTokenValue = refreshToken.value// 여기서 refresh token을 가져오세요
+                Log.d(LOG, "토큰을 적재합니다 (loadForRequest) : $refreshTokenValue")
 
                 // Add your Refresh Token to the cookies as a new Cookie object
                 val refreshTokenCookie = Cookie.Builder()
@@ -75,7 +76,7 @@ class HeartRateInstance : AppCompatActivity() {
         class myInterceptor : Interceptor {
             @Throws(IOException::class)
             override fun intercept(chain: Interceptor.Chain) : Response = with(chain) {
-                Log.d("HEART_RATE_INSTANCE", "엑세스 토큰 꺼낸 결과 : ${accToken.value}")
+                Log.d(LOG, "엑세스 토큰 꺼낸 결과 : ${accToken.value}")
                 val newRequest = request().newBuilder()
                     .addHeader("Authorization", "Bearer ${accToken.value}")
                     .build()
@@ -101,6 +102,8 @@ class HeartRateInstance : AppCompatActivity() {
         // val prefs = PreferenceUtil(SplashActivity.context as Context);
         val accToken = mutableStateOf<String>("");
         val refreshToken = mutableStateOf<String>("")
+
+        const val LOG = "HEART_RATE_INSTANCE";
     }
 
 

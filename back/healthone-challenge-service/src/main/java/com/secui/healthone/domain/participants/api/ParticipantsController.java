87a91be10ee3c9b163d5aa99682e7d4271db7659
White-Asher <dkxmpower999@naver.com>
2023-05-13 15:20:26 +0900

    feat: participant 도메인 TOKEN 적용

--- back/healthone-challenge-service/src/main/java/com/secui/healthone/domain/participants/api/ParticipantsController.java
+++ back/healthone-challenge-service/src/main/java/com/secui/healthone/domain/participants/api/ParticipantsController.java
@@ -4,6 +4,8 @@ import com.secui.healthone.domain.participants.dto.ParticipantsInsertDto;
 import com.secui.healthone.domain.participants.dto.ParticipantsResDto;
 import com.secui.healthone.domain.participants.service.ParticipantsService;
 import com.secui.healthone.global.response.RestApiResponse;
+import com.secui.healthone.global.util.HeaderUtil;
+import com.secui.healthone.global.util.TokenService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -23,16 +25,21 @@ import javax.validation.Valid;
 public class ParticipantsController {
 
     private final ParticipantsService participantsService;
+    private final TokenService tokenService;
 
     // 챌린지 참가
     @Operation(summary = "챌린지 참가 등록", description = "챌린지 참가 등록 API", tags = {"Participants"})
     @ApiResponses({@ApiResponse(responseCode = "200", description = "챌린지 참가 등록 성공", content = {
             @Content(mediaType = "application/json", schema = @Schema(implementation = ParticipantsInsertDto.class)),
             @Content(mediaType = "*/*", schema = @Schema(implementation = RestApiResponse.class)) }), })
+    @Parameter(name = "Authorization", description = "회원 Access Token", example = "Bearer access_token")
     @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "챌린지 등록 객체", required = true,
             content = @Content(schema = @Schema(implementation = ParticipantsInsertDto.class)))
     @PostMapping
-    public RestApiResponse<ParticipantsResDto> insert(@RequestBody ParticipantsInsertDto participantsInsertDto) {
+    public RestApiResponse<ParticipantsResDto> insert(@RequestHeader(required = false) String Authorization, @Valid @RequestBody ParticipantsInsertDto participantsInsertDto) {
+        String accessToken = HeaderUtil.getAccessTokenString(Authorization);
+        Integer userNo = tokenService.getUserNo(accessToken);
+        participantsInsertDto.setUserNo(userNo);
         return new RestApiResponse<>("챌린지 참가 등록 성공", participantsService.insert(participantsInsertDto));
     }
 
@@ -41,10 +48,13 @@ public class ParticipantsController {
     @ApiResponses({@ApiResponse(responseCode = "200", description = "챌린지 참가 취소 성공", content = {
             @Content(mediaType = "application/json", schema = @Schema(implementation = ParticipantsInsertDto.class)),
             @Content(mediaType = "*/*", schema = @Schema(implementation = RestApiResponse.class)) }), })
+    @Parameter(name = "Authorization", description = "회원 Access Token", example = "Bearer access_token")
     @Parameter(name = "no", description = "참가번호", example = "1")
     @DeleteMapping
-    public RestApiResponse<Void> delete(@Valid @RequestParam("no") Integer no) {
-        participantsService.delete(no);
+    public RestApiResponse<Void> delete(@RequestHeader(required = false) String Authorization, @Valid @RequestParam("no") Integer no) {
+        String accessToken = HeaderUtil.getAccessTokenString(Authorization);
+        Integer userNo = tokenService.getUserNo(accessToken);
+        participantsService.delete(no, userNo);
         return new RestApiResponse<>("챌린지 참가 취소 성공", null);
     }
 
